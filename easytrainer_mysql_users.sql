ec5365c0427d0d5bf4501cae449576033cbcb985
Matheus <matheusfarnetani@gmail.com>
2025-05-01 01:50:08 -0300

    dev300425

--- easytrainer_mysql_users.sql
+++ easytrainer_mysql_users.sql
@@ -2,6 +2,9 @@
 REVOKE ALL PRIVILEGES, GRANT OPTION FROM 'easytrainer_admin'@'%';
 REVOKE ALL PRIVILEGES, GRANT OPTION FROM 'easytrainer_instructor'@'%';
 REVOKE ALL PRIVILEGES, GRANT OPTION FROM 'easytrainer_user'@'%';
+REVOKE ALL PRIVILEGES, GRANT OPTION FROM 'easytrainer_admin'@'localhost';
+REVOKE ALL PRIVILEGES, GRANT OPTION FROM 'easytrainer_instructor'@'localhost';
+REVOKE ALL PRIVILEGES, GRANT OPTION FROM 'easytrainer_user'@'localhost';
 FLUSH PRIVILEGES;
 
 -- Excluir os usuários se já existirem (opcional, cuidado!)
@@ -9,22 +12,39 @@ DROP USER IF EXISTS 'easytrainer_admin'@'%';
 DROP USER IF EXISTS 'easytrainer_instructor'@'%';
 DROP USER IF EXISTS 'easytrainer_user'@'%';
 
+DROP USER IF EXISTS 'easytrainer_admin'@'localhost';
+DROP USER IF EXISTS 'easytrainer_instructor'@'localhost';
+DROP USER IF EXISTS 'easytrainer_user'@'localhost';
+
 -- Criar os usuários
-CREATE USER 'easytrainer_admin'@'%' IDENTIFIED BY 'senhaSegura1!';
-CREATE USER 'easytrainer_instructor'@'%' IDENTIFIED BY 'senhaSegura2!';
-CREATE USER 'easytrainer_user'@'%' IDENTIFIED BY 'senhaSegura3!';
+CREATE USER 'easytrainer_admin'@'%' IDENTIFIED WITH mysql_native_password BY 'senhaSegura1!';
+CREATE USER 'easytrainer_instructor'@'%' IDENTIFIED WITH mysql_native_password BY 'senhaSegura2!';
+CREATE USER 'easytrainer_user'@'%' IDENTIFIED WITH mysql_native_password BY 'senhaSegura3!';
+
+CREATE USER 'easytrainer_admin'@'localhost' IDENTIFIED WITH mysql_native_password BY 'senhaSegura1!';
+CREATE USER 'easytrainer_instructor'@'localhost' IDENTIFIED WITH mysql_native_password BY 'senhaSegura2!';
+CREATE USER 'easytrainer_user'@'localhost' IDENTIFIED WITH mysql_native_password BY 'senhaSegura3!';
 
 -- Conceder privilégios para o administrador
 GRANT ALL PRIVILEGES ON easytrainer.* TO 'easytrainer_admin'@'%';
 GRANT SELECT ON easytrainer_log.* TO 'easytrainer_admin'@'%';
 
+GRANT ALL PRIVILEGES ON easytrainer.* TO 'easytrainer_admin'@'localhost';
+GRANT SELECT ON easytrainer_log.* TO 'easytrainer_admin'@'localhost';
+
 -- Conceder privilégios ao instrutor (acesso completo a dados, mas sem alterar estrutura)
 GRANT SELECT, INSERT, UPDATE ON easytrainer.* TO 'easytrainer_instructor'@'%';
+
+GRANT SELECT, INSERT, UPDATE ON easytrainer.* TO 'easytrainer_instructor'@'localhost';
 
 -- Conceder privilégios ao usuário final:
 GRANT SELECT ON easytrainer.* TO 'easytrainer_user'@'%';
 GRANT UPDATE(name, mobile_number, weight, height, password)
   ON easytrainer.user TO 'easytrainer_user'@'%';
+
+GRANT SELECT ON easytrainer.* TO 'easytrainer_user'@'localhost';
+GRANT UPDATE(name, mobile_number, weight, height, password)
+  ON easytrainer.user TO 'easytrainer_user'@'localhost';
 
 -- Aplicar todas as permissões
 FLUSH PRIVILEGES;
@@ -33,3 +53,10 @@ FLUSH PRIVILEGES;
 SHOW GRANTS FOR 'easytrainer_admin'@'%';
 SHOW GRANTS FOR 'easytrainer_instructor'@'%';
 SHOW GRANTS FOR 'easytrainer_user'@'%';
+
+SHOW GRANTS FOR 'easytrainer_admin'@'localhost';
+SHOW GRANTS FOR 'easytrainer_instructor'@'localhost';
+SHOW GRANTS FOR 'easytrainer_user'@'localhost';
+
+
+SELECT user, host, plugin FROM mysql.user WHERE user = 'easytrainer_admin';
